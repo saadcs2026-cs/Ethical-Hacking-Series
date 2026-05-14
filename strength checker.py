f888e2144c31a8b5dd9ed9d3e64e77a8175fa33b
Muhammad Saad <saad.cs.2026@gmail.com>
2026-05-14 05:50:04 +0500

    Check password strength

---
+++ strength checker.py
@@ -0,0 +1,25 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def check_password(password):
+    score = 0
+    if len(password) >= 8:
+        score += 1
+    if any(c.isupper() for c in password):
+        score += 1
+    if any(c.islower() for c in password):
+        score += 1
+    if any(c.isdigit() for c in password):
+        score += 1
+    if any(c in "!@#$%" for c in password):
+        score += 1
+    return score
+
+password = input("Enter password: ")
+score = check_password(password)
+
+if score <= 2:
+    print("Weak password!")
+elif score <= 4:
+    print("Medium password!")
+else:
+    print("Strong password!")
